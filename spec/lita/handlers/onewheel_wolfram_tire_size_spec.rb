06ede32090667f62c3c32a7250b7d14f57f1f664
Andrew Kreps <andrew.kreps@gmail.com>
2019-09-20 08:10:27 -0700

    Updated test results x2

--- spec/lita/handlers/onewheel_wolfram_tire_size_spec.rb
+++ spec/lita/handlers/onewheel_wolfram_tire_size_spec.rb
@@ -11,16 +11,16 @@ describe Lita::Handlers::OnewheelTireSize, :lita_handler => true do
 
   it 'will work' do
     send_command 'tire 245 40 R17'
-    expect(replies.last).to eq('diameter for 245/40R17 is 24.72 inches')
+    expect(replies.last).to eq('diameter for 245 40 R17 is 24.72 inches')
   end
 
   it 'will work' do
     send_command 'tire 245 40 17'
-    expect(replies.last).to eq('diameter for 245/40R17 is 24.72 inches')
+    expect(replies.last).to eq('diameter for 245 40 17 is 24.72 inches')
   end
 
   it 'will work' do
     send_command 'tire 245/40 17'
-    expect(replies.last).to eq('diameter for 245/40R17 is 24.72 inches')
+    expect(replies.last).to eq('diameter for 245/40 17 is 24.72 inches')
   end
 end
